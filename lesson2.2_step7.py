4f1bb9e4d9a105398a0ccca335caacd56418ff81
Sai <110340558+lucsai@users.noreply.github.com>
2023-11-18 15:01:29 +0700

    Delete lesson2.2_step7.py

--- lesson2.2_step7.py
+++
@@ -1,6 +0,0 @@
-import os
-
-current_dir = os.path.abspath(os.path.dirname(__file__))
-file_path = os.path.join(current_dir, 'file.txt')
-print(os.path.abspath(__file__))
-print(os.path.abspath(os.path.dirname(__file__)))
